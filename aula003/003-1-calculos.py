8bd38253bf7cacb8e0125f91c746ff7d757350be
Maykon Granemann <maykondgranemann@gmail.com>
2025-12-10 22:29:57 -0300

    Aula003 - Arquivos da aula 003

---
+++ aula003/003-1-calculos.py
@@ -0,0 +1,16 @@
+numero1 = float(input('Digite um numero: '))
+numero2 = float(input('Digite outro numero: '))
+
+soma = numero1 + numero2
+subt = numero1 - numero2
+mult = numero1 * numero2
+divi = numero1 / numero2
+expo = numero1 ** numero2
+resto = numero1 % numero2
+
+print(f'Soma: {soma}')
+print(f'Subtracao: {subt}')
+print(f'Multiplicacao: {mult}')
+print(f'Divisao: {divi}')
+print(f'Exponenciacao: {expo}')
+print(f'Resto da Divisao: {resto}')
